5aee84b0670d089cec990aa55ba396f6491c9438
Mandy Nijssen <nijssenmandy89@gmail.com>
2020-12-05 13:11:31 +0100

    feat: autocomplete on search channel

--- src/components/Dashboard/SideBar/SideBar.tsx
+++ src/components/Dashboard/SideBar/SideBar.tsx
@@ -12,7 +12,7 @@ import {
   DrawerOverlay,
   DrawerCloseButton,
   DrawerContent,
-  Input,
+  Text,
 } from '@chakra-ui/react';
 import * as actions from '../../../store/actionCreators';
 import * as apiClientServer from '../../../helpers/apiClientServer';
@@ -33,7 +33,16 @@ const SideBar: React.FC<Props> = ({ showSideBar, setShowSideBar }) => {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [allChannels, setAllChannels] = useState<Channel[]>();
+  const [allChannels, setAllChannels] = useState<Channel[]>([]);
+  const [value, setValue] = useState<Channel | null>({
+    'id': 'f8d71201-8a5f-4cd5-9989-46242eb4b49c',
+    'name': 'Electronic',
+    'ownerId': null,
+    'private': false,
+    'parentId': null,
+    posts: []
+  });
+  const [searchResult, setSearchResult] = useState<string>('');
 
   useEffect(() => {
     if (userDetails.id) {
@@ -43,13 +52,17 @@ const SideBar: React.FC<Props> = ({ showSideBar, setShowSideBar }) => {
     }  
   }, []);
 
-  // useEffect(() => {
-  //   apiClientServer.getPublicChannels()
-  //     .then((result) => setAllChannels(result));
-  // }, []);
+  useEffect(() => {
+    apiClientServer.getPublicChannels()
+      .then((result) => {
+        setAllChannels(result);
+      });
+  }, []);
 
   const changePage = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> 
+    | React.MouseEvent<HTMLDivElement, MouseEvent>
+    | React.FormEvent<HTMLFormElement>,
     newChannel: Channel
   ) => {
     dispatch(actions.addCurrChannel(newChannel));
@@ -65,6 +78,27 @@ const SideBar: React.FC<Props> = ({ showSideBar, setShowSideBar }) => {
     if (showSideBar) onOpen();
   }, [showSideBar, onOpen]);
 
+  // const handleChange = (
+  //   e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  //   | React.ChangeEvent<{}>) => {
+  //   const target = e.target as HTMLTextAreaElement ;
+  //   const newSearch = target.value;
+  //   console.log('new search', newSearch);
+  //   setSearchResult(newSearch);
+  // };
+
+  const handleClickAutocomplete = (
+    e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const channelToChange= allChannels
+      .filter((chan) => chan.name.toLowerCase() === searchResult.toLowerCase());
+
+    if (channelToChange[0] !== undefined) {
+      changePage(e, channelToChange[0]);
+    } 
+  };
+
+
   return (
     <div>
       <Drawer isOpen={isOpen} placement="left" onClose={onClose} size="full">
@@ -75,20 +109,33 @@ const SideBar: React.FC<Props> = ({ showSideBar, setShowSideBar }) => {
             <DrawerBody>
               <div className="drawer_channel">Channels</div>
 
-              {/* <form>
-                <Input
-                  className="search_input"
-                  placeholder="Search a channel"
-                  variant="filled"
+              <form 
+                onSubmit={(e) => handleClickAutocomplete(e)}
+                className="drawer_search"
+              >
+                <Autocomplete
+                  inputValue={searchResult}
+                  onInputChange={(e, newInput) => {
+                    console.log(newInput);
+                    setSearchResult(newInput);
+                  }}
+                  value={value}
+                  onChange={(e, newValue: Channel | null) => {
+                    setValue(newValue);
+                  }}
+                  id="size-small-standard"
+                  size="small"
+                  options={allChannels}
+                  getOptionLabel={(option) => option.name}
+                  style={{ width: '81%', border: 'none' }}
+                  renderInput={(params) =>
+                    <TextField
+                      {...params}
+                      placeholder='Search for a Channel'
+                      // onChange={(e) => handleChange(e)}
+                    />}
                 />
-              </form> */}
-              {/* <Autocomplete
-                id="search-channel"
-                options={allPublicChannels}
-                getOptionLabel={(option) => option.title}
-                style={{ width: 300 }}
-                renderInput={(params) => <TextField {...params} label="Search for a Channel" variant="outlined" />}
-              /> */}
+              </form>
 
               <div className="drawer_public">
                 <h3 className="public_title">Public</h3>
