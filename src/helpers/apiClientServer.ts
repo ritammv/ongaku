cce0ae093b21376209608fc666df7a712383477b
Ritam Verma <ritamvvv@gmail.com>
2020-12-06 12:40:05 +0100

    ongaku logo trial

--- src/helpers/apiClientServer.ts
+++ src/helpers/apiClientServer.ts
@@ -171,32 +171,31 @@ const createPost = (
   user: User,
   postForm: FinalPost
 ) => {
-  return fetchRequest(release.url)
-    .then((moreInfo) => { 
-      console.log(moreInfo);
-      console.log(release);
-      const dbPost = {
-        userId: user.id,
-        channelId,
-        url: moreInfo.resource_url,
-        postTitle: postForm.message_title,
-        title: moreInfo.title,
-        artist: moreInfo.artists[0].name,
-        year: moreInfo.year,
-        label: release.labels[0],
-        body: postForm.message_body,
-        thumbnail: release.image,
-        // masterUrl: moreInfo.master_url
-      };
-      return fetchRequest(`${BASE_URL}/posts/${channelId}`, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(dbPost),
-      });
+  return fetchRequest(release.url).then((moreInfo) => {
+    console.log(moreInfo);
+    console.log(release);
+    const dbPost = {
+      userId: user.id,
+      channelId,
+      url: moreInfo.resource_url,
+      postTitle: postForm.message_title,
+      title: moreInfo.title ? moreInfo.title : null,
+      artist: moreInfo.artists ? moreInfo.artists[0].name : null,
+      year: moreInfo.year ? moreInfo.year : null,
+      label: release.labels ? release.labels[0] : null,
+      body: postForm.message_body,
+      thumbnail: release.image ? release.image : null,
+      // masterUrl: moreInfo.master_url
+    };
+    return fetchRequest(`${BASE_URL}/posts/${channelId}`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dbPost),
     });
+  });
 };
 
 const removePost = (postId: string, userId: number) => {
